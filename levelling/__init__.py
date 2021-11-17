8281482e5d0a98bcc8572d4cca44b667a4967069
Ethan <ethan@koldfusion.xyz>
2021-11-17 18:17:00 +1300

    Bump version

--- levelling/__init__.py
+++ levelling/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 import logging
 from collections import namedtuple
@@ -8,4 +8,4 @@ from levelling.payloads import LevelUpPayload
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
-version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="alpha", serial=0)
+version_info = VersionInfo(major=0, minor=1, micro=1, releaselevel="alpha", serial=0)
