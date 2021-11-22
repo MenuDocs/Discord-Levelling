82a110f00f0ecf6b0941da83a727a0ad10919c71
skelmis <skelmis.craft@gmail.com>
2021-11-22 22:20:55 +1300

    Document options

--- levelling/options.py
+++ levelling/options.py
@@ -1,9 +1,39 @@
-from dataclasses import dataclass
+import attr
 
 
-@dataclass
+@attr.s(slots=True)
 class Options:
-    per_guild: bool = True
-    ignore_dms: bool = True
-    ignore_bots: bool = True
-    xp_base_increase_amount: int = 5
+    """
+    The levelling instance's options
+
+    Parameters
+    ----------
+    per_guild: bool
+        Whether levelling is per guild or global.
+
+        Per guild by default.
+    ignore_dms: bool
+        Whether or not to ignore messages in dms as
+        part of the levelling system.
+
+        Default is True, so messages don't count as xp
+    ignore_bots: bool
+        Are bots also doing levelling?
+
+        Default is no
+    xp_base_increase_amount: int
+        How much xp each message is worth as a base
+
+        Logic is ``math.floor(random.random() * 10) + xp_base_increase_amount``
+    """
+
+    per_guild: bool = attr.ib(default=True, validator=attr.validators.instance_of(bool))
+    ignore_dms: bool = attr.ib(
+        default=True, validator=attr.validators.instance_of(bool)
+    )
+    ignore_bots: bool = attr.ib(
+        default=True, validator=attr.validators.instance_of(bool)
+    )
+    xp_base_increase_amount: int = attr.ib(
+        default=5, validator=attr.validators.instance_of(int)
+    )
